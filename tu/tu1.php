2601673a49c8c76747f6d582862bad38c03c340a
vipva <53024590+vipva@users.noreply.github.com>
2020-08-31 17:57:33 +0800

    Delete tu1.php

--- tu/tu1.php
+++
@@ -1,8 +0,0 @@
-<?php 
-//取得文件夹中图片数组 
-$img_array = glob("./tu1/*.{gif,jpg,png}",GLOB_BRACE); 
-//从数组中选择一个随机图片 
-$img = array_rand($img_array); 
-//在页面显示图片 
-echo '<img alt="'.$img_array[$img].'" src="'.$img_array[$img].'" />'; 
-?
